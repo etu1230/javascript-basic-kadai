995479e5fa19966750c12ff2f368ae9f3c218a61
etu1230 <kina.mochi.1230@gmail.com>
2025-01-04 18:06:34 +0900

    Add files via upload

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,7 @@
+const today = new Date();
+const year = today.getFullYear();
+const month = today.getMonth() + 1;
+const date = today.getDate();
+
+const dateToday = `${year}年${month}月${date}日`;
+console.log(dateToday);
